2da7721ab43dc8d5678e9621587fb9d46b0f504b
Kawsar Ahmed <kawcherahmed43@gmail.com>
2020-11-25 01:49:30 +0600

    Update settings.py

--- trvaeltour/settings.py
+++ trvaeltour/settings.py
@@ -137,10 +137,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'build_381ad260_', 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'trvaelblogs', 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR,'build_381ad260_', 'static'),
+   os.path.join(BASE_DIR,'trvaelblogs', 'static'),
 
 ]
 MEDIA_URL='/media/'
